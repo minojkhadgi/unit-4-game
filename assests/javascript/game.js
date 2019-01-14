d570172849afb8e399983ee7908a3fa68e1dcad8
minojkhadgi <gwminoj@gmail.com>
2019-01-14 17:59:11 -0500

    new

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,73 +1,101 @@
-var randNumber = Math.floor((Math.random()* 100) + 19);
-var red = Math.floor((Math.random()* 12) + 1);
-var blue = Math.floor((Math.random()* 12) + 1);
-var yellow = Math.floor((Math.random()* 12) + 1);
-var green = Math.floor((Math.random()* 12) + 1);
+$(document).ready(function () {
+    //Generates the random number by computer to match..
+    var comPick = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+    $('.randNumber').html(comPick);
 
-var wins = 0;
-var losses = 0;
-var totalScore = 0;
+    //generates the random number whenever click on crystals..
+    var colBlue = Math.floor(Math.random() * 12 + 1)
+    var colGreen = Math.floor(Math.random() * 12 + 1)
+    var colRed = Math.floor(Math.random() * 12 + 1)
+    var colYellow = Math.floor(Math.random() * 12 + 1)
 
-var updateAddition = function(){
-    $(".randNumber").empty();
-    $(".randNumber").append(randNumber);
-    $(".totalScore").empty();
-    $(".totalScore").append(totalScore);
-    $('#wins').empty();
-	$('#wins').append(wins);
-	$('#losses').empty();
-	$('#losses').append(losses);}
-	
-var restart = function(){
-    totalScore = 0;
-    randNumber = Math.floor((Math.random()* 100) + 19);
+    //every new game start with zero
+    var playerTotal = 0;
+    var wins = 0;
+    var losses = 0;
 
-    $(".randNumber").empty();
-    $(".randNumber").append(randNumber);
+    //updates addition win or loss.
+    $("#wins").text(wins);
+    $("#losses").text(losses);
 
-    red = Math.floor((Math.random()* 12) + 1);
-    blue = Math.floor((Math.random()* 12) + 1);
-    yellow = Math.floor((Math.random()* 12) + 1);
-    green = Math.floor((Math.random()* 12) + 1);
+    //creating restart function..
+    function reset() {
+        comPick = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+        $('.randNumber').html(comPick);
 
-    updateAddition();
-}
 
-var key = function(){
-    if ( totalScore == randNumber){
-        wins = wins +1;
-        restart();
+        colBlue = Math.floor(Math.random() * 12 + 1)
+        colGreen = Math.floor(Math.random() * 12 + 1)
+        colRed = Math.floor(Math.random() * 12 + 1)
+        colYellow = Math.floor(Math.random() * 12 + 1)
+        playerTotal = 0;
+        $('.totalScore').text(0);
     }
-    else if ( totalScore > randNumber){
-        losses = losses +1;
-        restart();
+    //adds the wins to the userTotal
+    function winPlus(){
+    alert("You won!");
+      wins++; 
+      $("#wins").text(wins);
+      reset();
     }
-    else{
-        updateAddition();
+    //addes the losses to the userTotal
+    function losePlus(){
+    alert ("You lose!");
+      losses++;
+      $("#losses").text(losses);
+      reset()
     }
-}
-$(".randNumber").append(randNumber);
-$(".totalScore").append(totalScore);
 
-$(document).ready(function(){
-$("#red").click(function(){
-    totalScore = totalScore + red;
-    key();
-})
-$("#blue").click(function(){
-    totalScore = totalScore + blue;
-    key();
-})
-$("#yellow").click(function(){
-    totalScore = totalScore + yellow;
-    key();
-})
-$("#green").click(function(){
-    totalScore = totalScore + green;
-    key();
-})
+    //activating on click function on crystals
+    // blue crystal
+    $('#blue').on('click', function() {
+        playerTotal = playerTotal + colBlue;
+        $('.totalScore').text(playerTotal);
 
+        if (playerTotal === comPick) {
+            winPlus();
+        } else if (playerTotal > comPick) {
+            losePlus();
+        }
+    });
+
+    //green crystal
+    $('#green').on('click', function() {
+        playerTotal = playerTotal + colGreen;
+        $('.totalScore').text(playerTotal);
+
+        if (playerTotal === comPick) {
+            winPlus();
+        } else if (playerTotal > comPick) {
+            losePlus();
+        }
+    });
+
+    //red crystal
+    $('#red').on('click', function() {
+        playerTotal = playerTotal + colRed;
+        $('.totalScore').text(playerTotal);
+
+        if (playerTotal === comPick) {
+            winPlus();
+        } else if (playerTotal > comPick) {
+            losePlus();
+        }
+    });
+
+    // yellow crystals
+    $('#yellow').on('click', function() {
+        playerTotal = playerTotal + colYellow;
+        $('.totalScore').text(playerTotal);
+
+        if (playerTotal === comPick) {
+            winPlus();
+        } else if (playerTotal > comPick) {
+            losePlus();
+        }
+    });
 });
+
 
 
 
